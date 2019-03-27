39b1820bdf172a90cf4ebbd61673df8f50a56461
vanit4s <szymon.kowalsky@hotmail.com>
2019-03-27 20:52:50 +0000

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private float speed = 10f;
+    private float jumpForce = 8f;
+    private float gravity = 30f;
+    private Vector3 moveDir = Vector3.zero;
+
+    void Start()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        CharacterController controller = gameObject.GetComponent<CharacterController>();
+
+        if (controller.isGrounded)
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDir = transform.TransformDirection(moveDir);
+            moveDir *= speed;
+            
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDir.y = jumpForce;
+            }
+        }
+
+        moveDir.y -= gravity * Time.deltaTime;
+    }
+
+}
